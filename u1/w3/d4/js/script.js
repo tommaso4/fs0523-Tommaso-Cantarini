19284b9f6bea7c7080960c5a23babc34cead472f
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-10-12 19:13:02 +0200

    add-background to number called

--- u1/w3/d4/js/script.js
+++ u1/w3/d4/js/script.js
@@ -6,24 +6,26 @@ let estratti = [];
 for (let i = 1; i <= 76; i++) {
     let cella = document.createElement('div');
     cella.classList.add('cella');
-    cella.classList.add(i);
     cella.innerText = i;
     tabellone.append(cella);
+    cella.id = `cel${i}`;
 }
 
 
-btnGeneratore.addEventListener('click',function () {
-    
+
+btnGeneratore.addEventListener('click', function () {
+
     let randomNum = Math.floor(Math.random() * 76) + 1;
-    estratti.push(randomNum);  
-    for (const el of cella.className) {
-        if (el == randomNum) {
-            cella.classList.add('newEstratto')
-        }
+    let cella = document.querySelector('#cel' + randomNum);
+    estratti.push(randomNum);
+    if (cella) {
+        cella.classList.add('newEstratto')
     }
 })
 
-let cella = document.querySelectorAll('.cella');
+
+console.log(estratti);
+
 
 
 
