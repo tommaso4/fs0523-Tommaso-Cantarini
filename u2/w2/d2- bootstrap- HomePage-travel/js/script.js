94e44eb92946ce79489672c70aa6b8f51f75efa8
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-11-01 12:04:22 +0100

    add: aria form

---
+++ u2/w2/d2- bootstrap- HomePage-travel/js/script.js
@@ -0,0 +1,9 @@
+// ------------------------- count the journey -----------------------const
+
+const journey = document.querySelectorAll('.nr-journey')
+
+console.log(journey.length);
+
+const spanNumJourney = document.querySelector('#num-journey')
+spanNumJourney.innerHTML = journey.length
+console.log(spanNumJourney);
