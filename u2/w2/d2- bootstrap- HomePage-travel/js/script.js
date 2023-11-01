352f56cf272a4c064ae1273732b17df9b8a3652f
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-11-01 12:45:16 +0100

    feat: add badge, add function remove cards

--- u2/w2/d2- bootstrap- HomePage-travel/js/script.js
+++ u2/w2/d2- bootstrap- HomePage-travel/js/script.js
@@ -2,8 +2,26 @@
 
 const journey = document.querySelectorAll('.nr-journey')
 
-console.log(journey.length);
-
 const spanNumJourney = document.querySelector('#num-journey')
 spanNumJourney.innerHTML = journey.length
-console.log(spanNumJourney);
+
+const createBadge = ()=>{
+    const badgeHot = document.querySelectorAll('.badge-hot')
+    badgeHot.forEach((el)=>{
+        const myBadge = document.createElement('div');
+        myBadge.innerHTML= `<span class="badge bg-secondary position-absolute top-0 end-0 m-3">HOT</span>`
+        el.classList.add('position-relative')
+        el.append(myBadge)
+    })
+}
+
+createBadge()
+
+const removeFunction = ()=>{
+    const myCards = document.querySelectorAll('.card')
+    myCards.forEach((card)=>{
+        card.remove();
+    })
+}
+
+// removeFunction()
