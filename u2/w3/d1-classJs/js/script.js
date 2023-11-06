53ef70f81beb50a932ff891c8ab1e25d8738597c
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-11-06 17:11:56 +0100

    class-js

--- u2/w3/d1-classJs/js/script.js
+++ u2/w3/d1-classJs/js/script.js
@@ -1,58 +1,99 @@
 
-class User{
-    constructor(name, surname, location, age){
-        this.name= name
-        this.surname= surname
-        this.location= location
-        this.age= age
+class User {
+    constructor(name, surname, location, age) {
+        this.name = name
+        this.surname = surname
+        this.location = location
+        this.age = age
     }
-    compareAge(anotherUser){
+    compareAge(anotherUser) {
         if (this.age > anotherUser.age) {
             console.log(`${this.name} è più grande di ${anotherUser.name}`)
-        } else if (this.age < anotherUser.age){
+        } else if (this.age < anotherUser.age) {
             console.log(`${anotherUser.name} è più grande di ${this.name}`)
-        } else{
+        } else {
             console.log(`${this.name} ha la stessa età di ${anotherUser.name}`)
         }
     }
 }
 
-let user1 = new User('Mario','Rossi', 'Milano',30)
-let user2 = new User('Luigi','Verdi', 'Padova', 30)
+let user1 = new User('Mario', 'Rossi', 'Milano', 30)
+let user2 = new User('Luigi', 'Verdi', 'Padova', 30)
 user2.compareAge(user1)
 
 // --------------------------------- exercise 2------------------------------------------
 
-class Pet {
-    constructor(){
-        this.petName= null
-        this.ownerName= null
-        this.species= null
-        this.breed= null
-        this.btnForm()
-    }
+function btnForm() {
+    const btnForm = document.querySelector('#btnForm')
+    btnForm.addEventListener("click", () => {
+        getValueHtml()
+        removeValue()
+    })
+}
 
-    btnForm(){
-        this.btnForm = document.querySelector('#btnForm')
-        this.btnForm.addEventListener("click", () => {
-            this.getValueHtml()
-            this.removeValue()
-        })
+function getValueHtml() {
+    let petName = document.querySelector('#petName').value
+    let ownerName = document.querySelector('#ownerName').value
+    let species = document.querySelector('#species').value
+    let breed = document.querySelector('#breed').value
+    let newPet = new Pet(petName, ownerName, species, breed)
+    createPetHtml(newPet)
+}
+
+function removeValue() {
+    const petName = document.querySelector('#petName').value = ''
+    const ownerName = document.querySelector('#ownerName').value = ''
+    const species = document.querySelector('#species').value = ''
+    const breed = document.querySelector('#breed').value = ''
+}
+
+function createPetHtml(petObj) {
+    let {petName, ownerName, species, breed} = petObj
+    const areaNewPet = document.createElement('div')
+    areaNewPet.classList.add('border')
+    const area = document.querySelector('#area')
+    area.append(areaNewPet)
+    let pPet = document.createElement('p')
+    pPet.innerText = petName + ' ' + ownerName  + ' ' +  species  + ' ' + breed
+    console.log(petObj);
+    // for (const i of petObj) {
+    //     console.log(i);
+    // }
+    areaNewPet.append(pPet)
+}
+
+btnForm()
+
+class Pet {
+    constructor(petName, ownerName, species, breed) {
+        this.petName = petName
+        this.ownerName = ownerName
+        this.species = species
+        this.breed = breed
     }
-    getValueHtml(){
-        this.petName= document.querySelector('#petName').value
-        this.ownerName= document.querySelector('#ownerName').value
-        this.species= document.querySelector('#species').value
-        this.breed= document.querySelector('#breed').value
-    }
-    removeValue(){
-        this.petName= document.querySelector('#petName').value = ''
-        this.ownerName= document.querySelector('#ownerName').value = ''
-        this.species= document.querySelector('#species').value = ''
-        this.breed= document.querySelector('#breed').value = ''
+    controlOwner(pet) {
+        if (this.ownerName == pet.ownerName) {
+            const myDiv = document.createElement('div')
+            myDiv.innerText = `${this.petName} e ${pet.petName} hanno lo stesso padrone`
+            const area = document.querySelector('#area')
+            area.append(myDiv)
+        }
     }
 }
 
-let ciuffo= new Pet
-console.log(ciuffo)
-console.log(kaly);
+// class superPet{
+//     constructor() {
+//         this.petName =  null
+//         this.ownerName =  null
+//         this.species =  null
+//         this.breed = null
+//     }
+//     btnForm() {
+//         const btnForm = document.querySelector('#btnForm')
+//         btnForm.addEventListener("click", () => {
+//             getValueHtml()
+//             removeValue()
+//         })
+//     }
+
+// }
