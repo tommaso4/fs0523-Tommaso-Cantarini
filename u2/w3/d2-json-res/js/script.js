58733efa0a3ca54a2223fc1deeaaad500efbf71d
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-11-07 17:00:06 +0100

    feat: prova oop

--- u2/w3/d2-json-res/js/script.js
+++ u2/w3/d2-json-res/js/script.js
@@ -26,9 +26,66 @@ if (storedUserArrJson) {
 
 console.log(myUserArr);
 
+// --------------------------------------------- es 2 -----------------------------
 
 
+function initializeCounter() {
+    const initialValue = 0;
+    sessionStorage.setItem('counterValue', initialValue.toString());
+}
+
+function startCounter() {
+    if (!sessionStorage.getItem('counterValue')) {
+        initializeCounter();
+    }
+
+    let counter = sessionStorage.getItem('counterValue');
+    
+    const counterElement = document.querySelector('#counter');
+    
+    function updateCounter() {
+        counter++;
+        counterElement.textContent = counter;
+        
+        sessionStorage.setItem('counterValue', counter.toString());
+    }
+    
+    setInterval(updateCounter, 1000);
+}
+ 
+startCounter();
 
 
+// -------------------------- prova es 2  oop-----------------------------------
 
+class stampCounter {
+    constructor(){
+        this.startCounter()
+    }
+    initializeCounter(){
+        const initialValue = 0
+        sessionStorage.setItem('counterValue', initialValue.toString())
+    }
+
+    startCounter(){
+        if (!sessionStorage.getItem('counterValue')) {
+            this.initializeCounter();
+        }
+
+        const counter = sessionStorage.getItem('counterValue')
+
+        const counterElement = document.querySelector('#counter')
+
+        setInterval(this.updateCounter(counter,counterElement),1000)
+    }
+
+    updateCounter(counter, counterElement){
+        counter++
+        counterElement.textContent = counter
+        sessionStorage.setItem('counterValue', counter)
+    }
+}
+
+// const myStartCounter = new stampCounter
+// myStartCounter
 
