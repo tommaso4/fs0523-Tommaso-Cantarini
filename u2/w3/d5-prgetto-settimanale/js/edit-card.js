dfa78b815e6ef301f369a2d32fe9a9a666391eb7
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-11-10 19:06:53 +0100

    .

--- u2/w3/d5-prgetto-settimanale/js/edit-card.js
+++ u2/w3/d5-prgetto-settimanale/js/edit-card.js
@@ -33,7 +33,7 @@ function genCArdClone(suit) {
     price.innerText = suit.price +'$';
 
     let areaHomePage = document.querySelector('#area-home-page');
-    areaHomePage.append(myDivArea);
+    areaHomePage.append(myDivArea); 
 }
 
 function showContent() {
@@ -42,3 +42,34 @@ function showContent() {
 }
 
 
+const mySuit = {
+    'name': 'suit',
+    'description': 'Good Choise',
+    'brand': 'Rip Curl',
+    'imageUrl': 'https://www.thenauticstore.com/5585-large_default/3mm-chest-zip-mute-da-surf-modello-2022-black.jpg',
+    'price': 100
+}
+
+const btnSave = document.querySelector('#btnSave')
+btnSave.addEventListener('click',function (e){
+    e.preventDefault()    
+    console.log('Hello World');
+    
+    mySuit.name = document.querySelector('.new-name').value
+    mySuit.imageUrl = document.querySelector('.img-url').value;
+    mySuit.price = document.querySelector('.new-price').value;
+    mySuit.brand = document.querySelector('.new-brand').value;
+    mySuit.description = document.querySelector('.new-description').value;
+    
+    fetch("https://striveschool-api.herokuapp.com/api/product/" + suitId, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZWY2ZjI1NGU4ODAwMTgzZjE4OTIiLCJpYXQiOjE2OTk2MDYzODMsImV4cCI6MTcwMDgxNTk4M30.CljDO05F7dJgoBljckBy5qrbKtGRxVYaM-pODs48lV0"
+        },
+        body: JSON.stringify(mySuit)
+    }).then(res => res.json())
+    .then( suits => {
+        location.href = '/index.html'
+    })
+})
