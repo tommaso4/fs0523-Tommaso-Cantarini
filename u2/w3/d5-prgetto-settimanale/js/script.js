7e2422b31a608aa943875e30acbdffcb7ce5e830
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-11-10 14:11:52 +0100

    feat: POST

--- u2/w3/d5-prgetto-settimanale/js/script.js
+++ u2/w3/d5-prgetto-settimanale/js/script.js
@@ -8,18 +8,6 @@ const mySuit = {
     'price': 100
 }
 
-fetch("https://striveschool-api.herokuapp.com/api/product/", {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZWY2ZjI1NGU4ODAwMTgzZjE4OTIiLCJpYXQiOjE2OTk2MDYzODMsImV4cCI6MTcwMDgxNTk4M30.CljDO05F7dJgoBljckBy5qrbKtGRxVYaM-pODs48lV0"
-    },
-    body: JSON.stringify(mySuit)
-})
-    .then(res => res.json())
-    .then(suits => {
-        console.log(suits);
-    })
 
 fetch("https://striveschool-api.herokuapp.com/api/product/", {
     headers: {
@@ -29,73 +17,79 @@ fetch("https://striveschool-api.herokuapp.com/api/product/", {
 })
     .then(res => res.json())
     .then(suits => {
-        console.log(suits);
-        suits.forEach(suit => {
-            let myDivArea = document.createElement('div');
-            myDivArea.classList.add('col-3')
-            let newCard = showContent();
-            myDivArea.append(newCard);
+        genCArdClone(suits)
 
-            let name = myDivArea.querySelector('.name-card');
-            let description = myDivArea.querySelector('.description');
-            let brand = myDivArea.querySelector('.brand');
-            let imageUrl = myDivArea.querySelector('.imageUrl');
-            let price = myDivArea.querySelector('.price');
-
-            name.innerText = suit.name;
-            description.innerText = suit.description;
-            brand.innerText = suit.brand;
-            imageUrl.src = suit.imageUrl;
-            price.innerText = suit.price;
-
-            let areaHomePage = document.querySelector('#area-home-page');
-            areaHomePage.append(myDivArea);
-
-        });
     });
 
+function genCArdClone(suits) {
 
-    fun
+    suits.forEach(suit => {
 
+        let myDivArea = document.createElement('div');
+        myDivArea.classList.add('col-3')
+        let newCard = showContent();
+        myDivArea.append(newCard);
 
+        let name = myDivArea.querySelector('.name-card');
+        let description = myDivArea.querySelector('.description');
+        let brand = myDivArea.querySelector('.brand');
+        let imageUrl = myDivArea.querySelector('.imageUrl');
+        let price = myDivArea.querySelector('.price');
 
-// fetch("https://striveschool-api.herokuapp.com/api/product/", {
-//     headers: {
-//         'Content-Type': 'application/json',
-//         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZWY2ZjI1NGU4ODAwMTgzZjE4OTIiLCJpYXQiOjE2OTk2MDYzODMsImV4cCI6MTcwMDgxNTk4M30.CljDO05F7dJgoBljckBy5qrbKtGRxVYaM-pODs48lV0"
-//     },
-// }).then(res => res.json())
-//     .then(suits => {
+        name.innerText = suit.name;
+        description.innerText = suit.description;
+        brand.innerText = suit.brand;
+        imageUrl.src = suit.imageUrl;
+        price.innerText = suit.price;
 
-//         suits.forEach(suit => {
+        let areaHomePage = document.querySelector('#area-home-page');
+        areaHomePage.append(myDivArea);
+    });
+}
 
-//             let myDivArea = document.createElement('div')
-//             let newCard = showContent()
-//             myDivArea.append(newCard)
+const btnSave = document.querySelector('#btnSave')
+btnSave.addEventListener('click', () => {
+    postNewObj()
+})
 
-//             let areaHomePage = document.querySelector('#area-home-page')
+function postNewObj() {
+    let newName = document.querySelector('.new-name').value;
+    let imgUrl = document.querySelector('.img-url').value;
+    let newPrice = document.querySelector('.new-price').value;
+    let newBrand = document.querySelector('.new-brand').value;
+    let newDescription = document.querySelector('.new-description').value;
 
-//             let name = myDivArea.document.querySelector('.name-card')
-//             let description = myDivArea.document.querySelector('.description')
-//             let brand = myDivArea.document.querySelector('.brand')
-//             let imageUrl = myDivArea.document.querySelector('.imageUrl')
-//             let price = myDivArea.document.querySelector('.price')
+    let newSuit = Object.assign({}, mySuit);
+    newSuit.name = newName;
+    newSuit.imageUrl = imgUrl;
+    newSuit.price = newPrice;
+    newSuit.brand = newBrand;
+    newSuit.description = newDescription;
 
-//             name.innerText = suit.name;
-//             description.innerText = suit.description;
-//             brand.innerText = suit.brand;
-//             imageUrl.src = suit.imageUrl;
-//             price.innerText = suit.price;
-
-//             areaHomePage.append(myDivArea)
-
-//         });
-
-//     })
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRkZWY2ZjI1NGU4ODAwMTgzZjE4OTIiLCJpYXQiOjE2OTk2MDYzODMsImV4cCI6MTcwMDgxNTk4M30.CljDO05F7dJgoBljckBy5qrbKtGRxVYaM-pODs48lV0"
+        },
+        body: JSON.stringify(newSuit)
+    }).then(res => {
+        if (!res.ok) {
+            throw new Error("Errore nella richiesta POST");
+        }
+        return res.json();
+    })
+        .then(suits => {
+            console.log(suits);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
 
 function showContent() {
-    let temp = document.getElementsByTagName("template")[0];
+    let temp = document.querySelector('#myTemp')
     return temp.content.cloneNode(true);
 }
 
