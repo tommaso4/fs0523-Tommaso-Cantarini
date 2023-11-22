e15d1502cfdd3f0b183e42d17f1ffe395bd1c218
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-11-22 16:53:21 +0100

    d3-w1

--- u3/w1-typescript/d3-tsc/script.js
+++ u3/w1-typescript/d3-tsc/script.js
@@ -1,15 +1,60 @@
 "use strict";
-// Scrivere un programma utilizzando la programmazione ad oggetti che calcoli le tasse che dovranno sostenere i
-// singoli lavoratori autonomi al fine di verificare il reddito annuo netto di ognuno a secondo del proprio reddito 
-// annuo lordo, tasse, cod redditività.Il programma utilizza classi astratte e non astratte.
-// Dati
-// Proprietà:
-// codredd: number;
-// redditoannuolordo: number
-// tasseinps: number
-// tasseirpef: number
-// Metodi:
-// getUtileTasse: number
-// getTasseInps: number
-// getTasseIrpef: number
-// getRedditoAnnuoNetto: number
+const arrUsers = [];
+class User {
+    constructor(id, name, surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+}
+const btnSave = document.querySelector('#btnSave');
+btnSave.addEventListener('click', (e) => {
+    e.preventDefault();
+    UserInps.addInfoNewUser();
+    console.log(arrUsers);
+});
+class UserInps extends User {
+    constructor(id, name, surname, codredd, redditoannuolordo) {
+        super(id, name, surname);
+        this.codredd = codredd;
+        this.redditoannuolordo = redditoannuolordo;
+        this.tasseinps = 26.3;
+        this.tasseirpef = 23;
+    }
+    static addInfoNewUser() {
+        const userName = document.querySelector('#userName');
+        const userSurname = document.querySelector('#userSurname');
+        const userId = document.querySelector('#userId');
+        const userCodredd = document.querySelector('#userCodredd');
+        const userRedditoannuolordo = document.querySelector('#userRedditoannuolordo');
+        const codere = Number(userCodredd.value);
+        const redditoannuolordo = Number(userRedditoannuolordo.value);
+        const name = userName.value;
+        const surname = userSurname.value;
+        const id = Number(userId.value);
+        const user = new UserInps(id, name, surname, codere, redditoannuolordo);
+        arrUsers.push(user);
+        userName.value = '';
+        userSurname.value = '';
+        userId.value = '';
+    }
+    getTasseInps() {
+        let utileMenoInps = this.redditoannuolordo * (this.tasseinps / 100);
+        return utileMenoInps;
+    }
+    getTasseIrpef() {
+        let utileMenoIrpef = this.redditoannuolordo * (this.tasseirpef / 100);
+        return utileMenoIrpef;
+    }
+    getUtileTasse() {
+        let utileTasse = this.getTasseInps() + this.getTasseIrpef();
+        return utileTasse;
+    }
+    getRedditoAnnuoNetto() {
+        let redditoAnnuoNetto = this.redditoannuolordo - this.getUtileTasse();
+        return redditoAnnuoNetto;
+    }
+}
+let userInps1 = new UserInps(1, 'Mario', 'Rossi', 40, 24000);
+console.log(userInps1.getTasseInps());
+//# sourceMappingURL=script.js.map
